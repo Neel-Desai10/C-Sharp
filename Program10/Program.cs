d2006fb2aa0950daceddccf1857d411889b3db19
Neel Desai <cr7nd7@gmail.com>
2024-03-04 23:30:15 +0530

    Program 10 modified

--- Program10/Program.cs
+++ Program10/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       Console.Write("Enter Roll Number: ");
+        int rollNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Marks of Subject 1: ");
+        int subject1Marks = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Marks of Subject 2: ");
+        int subject2Marks = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Marks of Subject 3: ");
+        int subject3Marks = Convert.ToInt32(Console.ReadLine());
+
+        int totalMarks = subject1Marks + subject2Marks + subject3Marks;
+
+        double percentage = (double)totalMarks / 3;
+
+        string division;
+        if (percentage >= 60)
+        {
+            division = "First";
+        }
+        else if (percentage >= 45)
+        {
+            division = "Second";
+        }
+        else if (percentage >= 33)
+        {
+            division = "Third";
+        }
+        else
+        {
+            division = "Fail";
+        }
+
+        Console.WriteLine("\nRoll Number: " + rollNumber);
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Total Marks: " + totalMarks);
+        Console.WriteLine("Percentage: " + percentage.ToString("0.00") + "%");
+        Console.WriteLine("Division: " + division);
     }
 }
